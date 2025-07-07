81c4cc8e12c4ac509cc9a7b30cf6ecadee0edc89
Zach Barnett <zacharybarnett96@gmail.com>
2025-07-06 18:32:17 -0700

    Finished JS; Cleaned up TODO flags; Updated "About Me" text

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,130 @@
-//TODO: Copy over course list (update COMPLETED field)
-//TODO: Setup event listeners for the course filter buttons
+const courses = [
+    {
+        subject: 'CSE',
+        number: 110,
+        title: 'Introduction to Programming',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+            'HTML',
+            'CSS'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+        technology: [
+            'C#'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 231,
+        title: 'Frontend Web Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    }
+]
+
+
+function certDisplayTemplate(certificate) {
+	let completion = "";
+	if (certificate.completed === true) {
+		completion = 'class="completed"'
+	}
+
+	return `<div ${completion}>${certificate.title}</div>`
+}
+
+function renderCertDisplay(certs) {
+	const htmlDivs = certs.map(certDisplayTemplate);
+	document.querySelector("#certificate-list").innerHTML = htmlDivs.join('\n');
+}
+
+const allBtn = document.querySelector("#all");
+const cseBtn = document.querySelector("#cse");
+const wddBtn = document.querySelector("#wdd");
+const credits = document.querySelector("#course-credits");
+
+let courseList = courses;
+renderCertDisplay(courseList);
+credits.textContent = courseList.reduce((credit, course) => credit + course.credits, 0);
+
+allBtn.addEventListener("click", () => {
+	allBtn.classList.add("active");
+	cseBtn.classList.remove("active");
+	wddBtn.classList.remove("active");
+	courseList = courses;
+	renderCertDisplay(courseList);
+	credits.textContent = courseList.reduce((credit, course) => credit + course.credits, 0);
+});
+
+cseBtn.addEventListener("click", () => {
+	allBtn.classList.remove("active");
+	cseBtn.classList.add("active");
+	wddBtn.classList.remove("active");
+	courseList = courses.filter((course) => course.subject.toLowerCase() === "cse");
+	renderCertDisplay(courseList);
+	credits.textContent = courseList.reduce((credit, course) => credit + course.credits, 0);
+});
+
+wddBtn.addEventListener("click", () => {
+	allBtn.classList.remove("active");
+	cseBtn.classList.remove("active");
+	wddBtn.classList.add("active");
+	courseList = courses.filter((course) => course.subject.toLowerCase() === "wdd");
+	renderCertDisplay(courseList);
+	credits.textContent = courseList.reduce((credit, course) => credit + course.credits, 0);
+});
