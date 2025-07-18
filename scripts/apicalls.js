6ef2e2567e6bc92fe23e49041c033f5137f5bd03
Zach Barnett <zacharybarnett96@gmail.com>
2025-07-17 20:35:12 -0700

    semi-colons....

--- scripts/apicalls.js
+++ scripts/apicalls.js
@@ -1,8 +1,8 @@
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
-const apiKey = ""
-const url = `https://api.openweathermap.org/data/2.5/weather/?lat=49.75&lon=6.63&appid=${apiKey}&units=imperial`
+const apiKey = "";
+const url = `https://api.openweathermap.org/data/2.5/weather/?lat=49.75&lon=6.63&appid=${apiKey}&units=imperial`;
 
 async function apiFetch() {
 	try {
