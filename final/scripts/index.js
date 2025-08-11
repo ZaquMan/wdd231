fd7916d9fbc2c326f39b8c9b150e20db32d2ac04
Zach Barnett <zacharybarnett96@gmail.com>
2025-08-10 18:55:12 -0700

    Home page HTML/CSS

---
+++ final/scripts/index.js
@@ -0,0 +1,22 @@
+const originsDiv = document.querySelector("#origins");
+const gameplayDiv = document.querySelector("#gameplay");
+const competitionDiv = document.querySelector("#competition");
+const modalBox = document.querySelector("#learning-modal");
+
+const card_content = {
+	"origins": {
+		"image": "images/mushi-ken-signs.webp",
+		"title": "",
+		"content": ""
+	},
+	"gameplay": {
+		"image": "images/rock-paper-scissors-diagram.webp",
+		"title": "",
+		"content": ""
+	},
+	"competition": {
+		"image": "images/rsp-2009-toronto-tournament.webp",
+		"title": "",
+		"content": ""
+	}
+}
--- final/scripts/nav.js
+++ final/scripts/nav.js
@@ -6,3 +6,22 @@ hamBtn.addEventListener("click", () => {
 	console.log("Clicked the Ham Nav Button")
 	hamNavMenu.classList.toggle("show");
 });
+
+// Shared Footer Elements
+const currentYear = document.querySelector("#currentYear");
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+const modifiedDate = new Date(document.lastModified);
+
+currentYear.textContent = today.getFullYear();
+lastModified.textContent = `${new Intl.DateTimeFormat(
+	"en-US", {
+		month: "short",
+		day: "2-digit",
+		year: "numeric"
+}).format(modifiedDate)} ${new Intl.DateTimeFormat(
+	"en-US", {
+			timeStyle: "short"
+		}
+	).format(modifiedDate)}`
